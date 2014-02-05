23fee38b62c3c0182ca7f0eeb9d1ae819569fbef
Eugene <eugene.galkine@gmail.com>
2014-02-05 10:46:57 -0500

    Update Input.h

--- Input.h
+++ Input.h
@@ -1,6 +1,3 @@
-// We might be able to use the DriverStationLCD class to find out where the errors are coming from.
-// How much does a new cRio cost?
-
 #ifndef _INPUT_H_
 #define _INPUT_H_
 
@@ -8,8 +5,7 @@
 #include "OctocanumDrive.h"
 
 // Buttons
-static const uint32_t MODE_TRACTION = 1;
-static const uint32_t MODE_MECANUM = 2;
+static const uint32_t MODE_TOGGLE = 1;
 
 // Axii
 static const uint32_t STRAFE_X = 1;
@@ -21,9 +17,9 @@ class Input
 {
 	Joystick * stick;
 	OctocanumDrive* drive;
-	public:
-		Input(Joystick* _stick, OctocanumDrive* _drive);
-		void Update();
+public:
+	Input(Joystick* _stick, OctocanumDrive* _drive);
+	void Update();
 };
 
 #endif
