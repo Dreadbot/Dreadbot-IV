74971fd95912be8b9ade49c391a9935390b829de
Sourec <tothemyers@gmail.com>
2014-02-08 11:54:34 -0600

    Add Shooter function definitions
    Added the function definitions in Shooter.cpp and modified Shooter.h to
    allow for shooting.

---
+++ Shooter.cpp
@@ -0,0 +1,41 @@
+#include "Shooter.h"
+
+Shooter::Shooter(Jaguar* newMotor, Encoder* newPot)
+{
+	winch = newMotor;
+	pot = newPot;
+}
+void Shooter::release()
+{
+	if (!active) return; //Do NOT release if the shooter is not prepared!
+	/* Code for releasing ball. I do not know how to do this.
+	Remove latch thingy?
+	*/
+	active = false;
+}
+bool Shooter::getActive()
+{
+	return active;
+}
+void Shooter::update()
+{	
+	position = pot->Get();	//Set position equal to encoder position.
+	if (!active) reset();
+}
+void Shooter::reset()
+{
+	if (position == SHOOTPOINT && !skip)
+	{ //If the winch has pulled the kicker back enough
+		skip = true;
+	}
+	else winch->Set(WINCHSPEED);
+	if (skip && (position > RESETPOS + WINCHERR || position < RESETPOS - WINCHERR))
+	{
+		winch->Set(-WINCHSPEED);
+	}
+	if (position > RESETPOS + WINCHERR || position < RESETPOS -WINCHERR) //Check if winch is reset
+	{
+		active = true;
+		skip = false;
+	}
+}
--- Shooter.h
+++ Shooter.h
@@ -1,20 +1,23 @@
 #include "WPILib.h"
 
+const int SHOOTPOINT = 100;	//Position at which winch has reset kicker
+const int WINCHSPEED = 1;	//Speed of winch
+const int RESETPOS = 0;		//Winch resting position
+const int WINCHERR = 5;		//Acceptable winch error
+
 class Shooter
 {
 public:
-	Shooter(Jaguar* newMotor, Encoder* newPot);
-	void release();
-	void moveTo(int position, int speed);
-	bool getActive();
-	bool reset();
-	void update();
+	Shooter(Jaguar* newMotor, Encoder* newPot);	//Constructor, gets a moter and encoder (pointer)
+	void release();								//Shoot the ball!
+	void moveTo(double position, int speed = 0);		//Move the shooter arm thingy to the given position. Can be used for autonomous/autoaiming
+	bool getActive();							//Is it active?
+	void reset();								//Reset the shooter (shift gears again?) for another shot
+	void update();								//Iterative function called in main robot class. Allows for pseudo-PID.
 private:
 	bool active;		//Is it active?
-	Jaguar* motor;		//Motor running winch
+	Jaguar* winch;		//Motor running winch
 	Encoder* pot;		//Encoder attatched to winch
-	int position;		//Current position
-	int newPos;			//Position to be moved to 
-	int countdown;		//Timer
-
+	double position;	//Current position
+	bool skip;			//temp bool
 }
