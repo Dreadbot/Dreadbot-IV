cda95ea56c83db62e55b16ac5b5dd7ad51517c98
Parker <parkerstebbins@gmail.com>
2014-02-13 23:08:44 -0500

    Update OctocanumDrive.h

--- OctocanumDrive.h
+++ OctocanumDrive.h
@@ -6,13 +6,48 @@
 
 float maxOutput = 0.5;
 
-struct OctocanumModule {
-	Solenoid *valve;
+struct OctocanumModule
+{
+	Valve *valve;
 	Talon *motor;
 	
-	OctocanumModule(uint8_t i)
+	OctocanumModule(uint8_t i, uint8_t vTop, uint8_t vBottom)
 	{
 		motor = new Talon(i);
+		valve = new Valve(vTop, vBottom);
+		
+	}
+};
+
+// This is actually a thing
+class Valve
+{
+	Solenoid *s0;
+	Solenoid *s1;
+	open = false;
+	
+public:
+	Valve(uint8_t p_s0, uint8_t p_s1)
+	{
+		s0 = new Solenoid(p_s0);
+		s1 = new Solenoid(p_s1);
+	}
+	
+	bool GetOpen() 
+	{
+		return open;
+	}
+	
+	void Open() 
+	{
+		s0->Set(true);
+		s1->Set(false);
+	}
+	
+	void Close() 
+	{
+		s0->Set(false);
+		s1->Set(true);	
 	}
 };
 
