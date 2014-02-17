bc0a0d590e353e2fe2590e36a162225786ed6ecc
Sourec <tothemyers@gmail.com>
2014-02-17 14:35:03 -0600

    Added Actually Useful Shooter Code
    Deleted the old, moronic code written (by me) and replaced with code
    that works properly. New code uses a limit switch rather than a
    potentiometer to detect if the shooter is ready.

--- Shooter.cpp
+++ Shooter.cpp
@@ -1,41 +1,40 @@
 #include "Shooter.h"
 
-Shooter::Shooter(Talon* _motor, Encoder* _pot)
+Shooter::Shooter(Talon* newMotor, DigitalInput* newSwitch, Solenoid* newSolenoid)
 {
-	winch = _motor;
-	pot = _pot;
-}
-void Shooter::release()
-{
-	if (!active) return; //Do NOT release if the shooter is not prepared!
-	/* Code for releasing ball. I do not know how to do this.
-	Remove latch thingy?
-	*/
 	active = false;
+	winchAtMax = false;
+	winchMotor = newMotor;
+	winchSwitch = newSwitch;
+	releaser = newSolenoid;
 }
-bool Shooter::getActive()
+void Shooter::shoot()
 {
-	return active;
-}
-void Shooter::update()
-{	
-	position = pot->Get();	//Set position equal to encoder position.
-	if (!active) reset();
+	if (!active) return; //Shooter cannot shoot while not ready
+	releaser->Set(false);
+	Wait(2.0);
+	releaser->Set(true);
+	active = false;
+	winchAtMax = false;
 }
 void Shooter::reset()
 {
-	if (position == SHOOTPOINT && !skip)
-	{ //If the winch has pulled the kicker back enough
-		skip = true;
-	}
-	else winch->Set(WINCHSPEED);
-	if (skip && (position > RESETPOS + WINCHERR || position < RESETPOS - WINCHERR))
+	if (active) return; //Shooter shouldn't reset if it is reset.
+	if (winchSwitch->Get()) winchAtMax;
+	if (!winchAtMax) winchMotor->Set(1);
+	if (winchAtMax)
 	{
-		winch->Set(-WINCHSPEED);
-	}
-	if (position > RESETPOS + WINCHERR || position < RESETPOS -WINCHERR) //Check if winch is reset
-	{
+		winchMotor->Set(-1);
+		Wait(4.0);
+		winchMotor->Set(0);
 		active = true;
-		skip = false;
 	}
+}
+void Shooter::update()
+{
+	reset();
+}
+bool getActive()
+{
+	return active;
 }
--- Shooter.h
+++ Shooter.h
@@ -1,24 +1,17 @@
 #include "WPILib.h"
 
-static const int SHOOTPOINT = 100;	//Position at which winch has reset kicker
-static const int WINCHSPEED = 1;	//Speed of winch
-static const int RESETPOS = 0;		//Winch resting position
-static const int WINCHERR = 5;		//Acceptable winch error
-
 class Shooter
 {
 public:
-	Shooter(Talon* _motor, Encoder* _pot);			//Constructor, gets a moter and encoder (pointer)
-	void release();									//Shoot the ball!
-	void moveTo(double position, int speed = 0);	//Move the shooter arm thingy to the given position. Can be used for autonomous/autoaiming
-	bool getActive();								//Is it active?
-	void reset();									//Reset the shooter (shift gears again?) for another shot
-	void update();									//Iterative function called in main robot class. Allows for pseudo-PID.
+	Shooter(Talon* newMotor, DigitalInput* newSwitch, Solenoid* newSolenoid);
+	void shoot();
+	void reset();
+	void update();
+	bool getActive();
 private:
-	bool active;		//Is it active?
-	Talon* winch;		//Motor running winch
-	Encoder* pot;		//Encoder attatched to winch
-	double position;	//Current position
-	bool skip;			//temp bool
+	bool active;
+	bool winchAtMax;
+	Talon* winchMotor;
+	DigitalInput* winchSwitch;
+	Solenoid* releaser;
 };
-
