35591fadf5d787b595f74fe83b108d732380fce3
Parker <parkerstebbins@gmail.com>
2014-02-20 17:28:52 -0500

    Update OctocanumDrive.h

--- OctocanumDrive.h
+++ OctocanumDrive.h
@@ -1,9 +1,11 @@
 #ifndef OCTOCANUMDRIVE_H_
 #define OCTOCANUMDRIVE_H_
 
+#define _PIDTUNE
+
 #include "WPILib.h"
+#include  "Valve.h"
 #include <math.h>
-#include "Valve.h"
 
 
 struct OctocanumModule
@@ -21,16 +23,16 @@ struct OctocanumModule
 		encoder->SetDistancePerPulse(1.44);
 			// for traction, 0.05027
 			// for mecanum, 0.07540
-		encoder->SetMinRate(2.0);
-		//encoder->SetPIDSourceParameter(2);
+		//encoder->SetMinRate(2);
+		encoder->SetPIDSourceParameter(encoder->kRate);
 
-		speedController = new PIDController(1.0, 1.0, 0.0, encoder, motor);
-		speedController->SetInputRange(-720.0, 720.0);
-		speedController->SetOutputRange(-1.0, 1.0);
+		speedController = new PIDController(0.1, 0, 0, encoder, motor, 0.05);
+		speedController->SetInputRange(720.0, -720.0);
+		speedController->SetOutputRange(-0.5, 0.5);
 	}
 };
 
-class OctocanumDrive 
+class OctocanumDrive
 {
 	OctocanumModule *drive[4];
 	bool tractionMode;
@@ -49,18 +51,19 @@ class OctocanumDrive
 
 
 	void SetOutputs(float leftOutput, float rightOutput);
-	void Normalize(double *wheelSpeeds);
+	void Normalize();
 	void RotateVector(double &x, double &y, double angle);
 	float Limit(float num);
 
 	void MechanumDrive(float x, float y, float rotation);
 	void ArcadeDrive(float moveValue, float rotateValue, bool squaredInputs);
 	
+	void Set();
 public:
 	Valve *valve0;
 	Valve *valve1;
-
-	float maxOutput;
+	
+	double maxOutput;
 	
 	OctocanumDrive();
 	void Enable();
