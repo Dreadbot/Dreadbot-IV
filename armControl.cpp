1065ead4646814d919a6b0c998e75fc1ab5a318a
Sourec <tothemyers@gmail.com>
2014-02-24 09:44:53 -0600

    Removed shootMode

--- armControl.cpp
+++ armControl.cpp
@@ -10,38 +10,19 @@ armControl::armControl(Talon* newWheels, Talon* newFlipper, DoubleSolenoid* newA
 }
 void armControl::moveArms(DoubleSolenoid::Value value)
 {
-	if (shooterMode != usrControl) return;
 	armPneu->Set(value);
 }
 void armControl::moveWheels(float value)
 {
-	if (shooterMode != usrControl) return; //Wheels cannot be moved in shootMode
 	armWheels->Set(value);
 }
 void armControl::moveFlipper(float value)
 {
-	if (shooterMode != usrControl) return; //Flipper cannot be moved in shootMode
-	//Possible flipper usage for aiming?
 	flipper->Set(value);
 }
 void armControl::setShootmode(mode newMode)
 {
 	shooterMode = newMode;
-}
-void armControl::update()
-{
-	if (shooterMode == usrControl) return; //usrControl doesn't need updating
-	if (shooterMode == holding)
-	{
-		if (flipPot->Get() > FLAPPYUP + ERRVAL)
-		{
-			flipper->Set(abs(flipPot->Get() - FLAPPYUP) / ERRVAL);
-		}
-		else if (flipPot->Get() < FLAPPYUP - ERRVAL)
-		{
-			flipper->Set(abs(flipPot->Get() + FLAPPYUP) / ERRVAL);
-		}
-	}
 }
 int armControl::getFlipPot()
 {
--- armControl.h
+++ armControl.h
@@ -1,24 +1,17 @@
 #include <WPILib.h>
 
-const int FLAPPYUP = 1000; //STANDIN 
-const int FLAPPYDOWN = 0;  //STANDIN
-const int ERRVAL = 15;
 
 class armControl
 {
 public:
-	enum mode {holding, usrControl};
 	armControl(Talon* newWheels, Talon* newFlipper, DoubleSolenoid* newArms, Encoder* newPot);
 	void moveArms(DoubleSolenoid::Value value);
 	void moveWheels(float value);
 	void moveFlipper(float value);
-	void setShootmode(mode newMode);
-	void update();
 	int getFlipPot();
 private:
 	Talon* armWheels;
 	Talon* flipper;
 	DoubleSolenoid* armPneu;
 	Encoder* flipPot;
-	mode shooterMode;
 };
