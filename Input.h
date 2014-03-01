f6d88511dab8041b4d490883112a88b7f47df89a
Sourec <tothemyers@gmail.com>
2014-03-01 11:14:57 -0600

    Added XMODE, Added DMODE

--- Input.h
+++ Input.h
@@ -3,58 +3,76 @@
 
 #include "WPIlib.h"
 #include "OctocanumDrive.h"
-#include "armControl.h"
+#include "ArmControl.h"
 #include "Shooter.h"
 
 #define PROFILE1
-#define DIRECTMODE_Lf310
+#define DIRECTMODE_DMODE
+#define DIRECTMODE_XMODE
 
-#ifdef DIRECTMODE_Lf310
-	#define BUTTON_A 2
-	#define BUTTON_B 3
-	#define BUTTON_X 1
-	#define BUTTON_Y 4
-	#define BUTTON_LB 5
-	#define BUTTON_RB 6
-	#define BUTTON_LT 7
-	#define BUTTON_RT 8
-	#define BUTTON_START 10
-	#define BUTTON_BACK 9
-	#define BUTTON_LJOY 11
-	#define BUTTON_RJOY 12
-	#define AXIS_LJOY_X 1
-	#define AXIS_LJOY_Y 2
-	#define AXIS_RJOY_X 3
-	#define AXIS_RJOY_Y 4
-	#define AXIS_DPAD_X 5
-	#define AXIS_DPAD_Y 6
+#ifdef DIRECTMODE_DMODE
+	#define D_BUTTON_A 2
+	#define D_BUTTON_B 3
+	#define D_BUTTON_X 1
+	#define D_BUTTON_Y 4
+	#define D_BUTTON_LB 5
+	#define D_BUTTON_RB 6
+	#define D_BUTTON_LT 7
+	#define D_BUTTON_RT 8
+	#define D_BUTTON_START 10
+	#define D_BUTTON_BACK 9
+	#define D_BUTTON_LJOY 11
+	#define D_BUTTON_RJOY 12
+	#define D_AXIS_LJOY_X 1
+	#define D_AXIS_LJOY_Y 2
+	#define D_AXIS_RJOY_X 3
+	#define D_AXIS_RJOY_Y 4
+	#define D_AXIS_DPAD_X 5
+	#define D_AXIS_DPAD_Y 6
+#endif
+
+#ifdef DIRECTMODE_XMODE
+	#define X_BUTTON_A 1
+	#define X_BUTTON_B 2
+	#define X_BUTTON_X 3
+	#define X_BUTTON_Y 4
+	#define X_BUTTON_LB 5
+	#define X_BUTTON_RB 6
+	#define X_BUTTON_START 8
+	#define X_BUTTON_BACK 7
+	#define X_BUTTON_LJOY 9	
+	#define X_BUTTON_RJOY 10
+	#define X_AXIS_TRIGGERS 3
+	#define X_AXIS_LJOY_X 1
+	#define X_AXIS_LJOY_Y 2
+	#define X_AXIS_RJOY_X 4
+	#define X_AXIS_RJOY_Y 5
+	#define X_AXIS_DPAD_X 6
 #endif
 
 #ifdef PROFILE1
 
 // Driver controls
-	#define MEC_X AXIS_LJOY_X 
-	#define MEC_Y AXIS_LJOY_Y
-	#define MEC_R AXIS_RJOY_X
-	#define ARC_Y AXIS_LJOY_Y
-	#define ARC_R AXIS_RJOY_X 
-	#define DROP BUTTON_RT
-	#define RAGE_MODE BUTTON_START
+	#define MEC_X D_AXIS_LJOY_X 
+	#define MEC_Y D_AXIS_LJOY_Y
+	#define MEC_R D_AXIS_RJOY_X
+	#define ARC_Y D_AXIS_LJOY_Y
+	#define ARC_R D_AXIS_RJOY_X 
+	#define DROP D_BUTTON_RT
+	#define RAGE_MODE D_BUTTON_START
 
 // Shooter controls
-	#define FLIPPER AXIS_RJOY_X //y
-	#define TRIGGER BUTTON_A
-	#define ARMS AXIS_LJOY_Y
-	#define ARM_MECS_IN BUTTON_LT
-	#define ARM_MECS_OUT BUTTON_RT
-	#define ARM_MECS AXIS_RJOY_Y
-	#define SHOOTER_RESET BUTTON_B
+	#define FLIPPER D_AXIS_RJOY_Y 
+	#define TRIGGER D_BUTTON_A
+	#define ARMS D_AXIS_LJOY_Y
+	#define ARM_MECS_IN D_BUTTON_LT
+	#define ARM_MECS_OUT D_BUTTON_RT
+	#define SHOOTER_RESET D_BUTTON_B
 
 // Dance controls (on driver joystick)
-	#define DANCE_ENABLE BUTTON_Y
-	#define DANCE_LEFT BUTTON_LB 
-	#define DANCE_RIGHT BUTTON_RB
-
+	#define DANCE_ENABLE D_BUTTON_Y
+	#define DANCE_LEFT D_BUTTON_LB 
+	#define DANCE_RIGHT D_BUTTON_RB
 #endif
 
 
